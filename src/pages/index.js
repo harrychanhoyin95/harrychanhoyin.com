3b3cbcaf2cdc96cd84807200db74cc4a005e7dd5
Harry Chan <Harry Chan>
2019-11-23 17:18:10 +0800

    initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import Layout from "../components/layout";
+
+const IndexPage = () => (
+  <Layout>
+    <div>Hello world</div>
+  </Layout>
+);
+
+export default IndexPage;
